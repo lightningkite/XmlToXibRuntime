57ff67fea7a9793881fb98d78b9335f05eb442f0
Shane Thompson <shane@meosphere.com>
2025-04-29 17:36:25 -0600

    Updated Package.swift, added Sources folder

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
             targets: ["XmlToXibRuntime"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/lightningkite/M13Checkbox.git", exact: "3.4.1-LK")
+    ]
     targets: [
         .target(
             name: "XmlToXibRuntime",
-            dependencies: [
-                .package(url: "https://github.com/lightningkite/M13Checkbox.git", exact: "3.4.1-LK")
-            ],
             path: "Sources/XmlToXibRuntime",
             publicHeadersPath: "include",
         ),
