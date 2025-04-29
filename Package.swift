5f766eeb9e51ef1c850838f4a785248675bc5a65
Shane Thompson <shane@meosphere.com>
2025-04-29 17:30:02 -0600

    Updated Package.swift, added Sources folder

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "XmlToXibRuntime",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "XmlToXibRuntime",
+            targets: ["XmlToXibRuntime"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/lightningkite/M13Checkbox.git", exact: "3.4.1-LK")
+    ],
+    targets: [
+        .target(
+            name: "XmlToXibRuntime",
+            dependencies: [],
+            path: "Sources/XmlToXibRuntime",
+            publicHeadersPath: "include",
+        ),
+        .testTarget(
+            name: "XmlToXibRuntimeTests",
+            dependencies: ["XmlToXibRuntime"],
+        ),
+    ],
+)
